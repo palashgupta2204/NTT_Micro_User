cb1b24492ca9ee58905ded43cda8a522e5b39d8b
Palash Gupta <palashgupta78@gmail.com>
2024-07-03 00:42:45 +0530

    Add files via upload

---
+++ src/main/java/com/example/user/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.user.repo;
+
+import com.example.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
+
