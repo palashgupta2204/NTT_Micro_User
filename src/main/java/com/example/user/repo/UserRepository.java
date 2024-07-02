5cf70f72e55fa7eab09e894ec1fe1b6c45991d84
Palash Gupta <palashgupta78@gmail.com>
2024-07-03 00:41:37 +0530

    Delete src/main directory

--- src/main/java/com/example/user/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.repo;
-
-import com.example.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
-
